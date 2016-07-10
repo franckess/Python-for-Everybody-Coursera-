b862d1b083b208a2fd7f601202d426be61f01311
Rene Franck <franckess48@gmail.com>
2016-07-10 19:31:47 +1000

    Pushing week 3, 4 & 5

---
+++ Getting Started with Python/Assignment_22.py
@@ -0,0 +1,7 @@
+## Assignment 2.2
+# Write a program that uses raw_input to prompt a user for their name and then welcomes them. Note that raw_input will pop up a dialog box
+
+# The code below almost works
+
+name = raw_input("Enter your name")
+print "Hello %s" % name
---
+++ Getting Started with Python/Assignment_23.py
@@ -0,0 +1,8 @@
+## Assignment 2.3
+# Write a program to prompt the user for hours and rate per hour using raw_input to compute gross pay. Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25).
+
+# This first line is provided for you
+
+hrs = float(raw_input("Enter Hours:"))
+rate = float(raw_input("Enter Rate:"))
+print hrs * rate
